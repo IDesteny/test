36894f153529c1cbdc1ec7f3793e0ace5c44e7c4
IDesteny <t1m0shenko@yandex.ru>
2020-10-05 00:40:23 +0400

    create func sortFind

--- test.cpp
+++ test.cpp
@@ -2,29 +2,44 @@
 using namespace std;
 
 template <class T>
-void sortFind(T *arr, const uint64_t &sizeArr)
+void sortFind(T *arr, const uint64_t &sizeArr, bool( *predicate)(const T &, const T &) = [](const T &arg1, const T &arg2) { return arg1 < arg2; })
 {
-	auto length = sizeArr / sizeof *arr;
-	
-	auto indexMaxItem = length, //for auto
-			i = length,
-			j = length;
+	auto length = sizeArr / sizeof *arr;	//get length array
+	auto i = length; 			//for auto
+	auto j = length; 			//for auto
+
+	T *item = arr; 				//for find
+	T temp; 				//for swap
 
 	for (i = 0; i < length - 1; ++i)
 	{
+		//find index max/min item
+		for (j = i; j < length; ++j)
+		{
+			if (predicate(*item, arr[j]))
+			{
+				item = arr + j;
+			}
+		}
 		
+		//swap
+		temp = *(arr + i);
+		arr[i] = *item;
+		*item = move(temp);
 	}
 }
 
 int main()
 {
-	int arr[] { 5, 3, 8, 3, 6, 2 };
+	int arr[] { 5, 3, 8, 0, 6, 7 };
 	
+	cout << "in  >>  ";
 	for (int i {}; i < 6; ++i) cout << arr[i] << ' ';
 	cout << endl;
 
 	sortFind(arr, sizeof arr);
 	
+	cout << "out <<  ";
 	for (int i {}; i < 6; ++i) cout << arr[i] << ' ';
 	cout << endl;
 
