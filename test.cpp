4765d372a0ee64bcb700c8bbdcd33d38266abecd
IDesteny <t1m0shenko@yandex.ru>
2020-10-04 22:22:44 +0400

    create array

--- test.cpp
+++ test.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int &&foo(int &&a)
-{
-	return move(a);
-}
-
 int main()
 {
-	cout << foo(5) << "change" << endl;
+	int arr[] { 5, 3, 8, 3, 6, 2 };
+	
+	for (int i {}; i < 6; ++i) cout << arr[i] << ' ';
+	cout << endl;
+
+	//TODO: create func
+	//sortFind();
+	
+	for (int i {}; i < 6; ++i) cout << arr[i] << ' ';
+	cout << endl;
+
 	return 0;
 }
